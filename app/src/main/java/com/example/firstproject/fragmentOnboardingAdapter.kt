fa27631efa0c8136b2e2fd91f5e351f8dd9b3f70
wan_m <morozko889@gmail.com>
2025-04-20 16:22:36 +0300

    Done

---
+++ app/src/main/java/com/example/firstproject/fragmentOnboardingAdapter.kt
@@ -0,0 +1,24 @@
+package com.example.firstproject
+
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.firstproject.fragment.FirstOnboardingFragment
+import com.example.firstproject.fragment.SecondOnboardingFragment
+import com.example.firstproject.fragment.ThirdOnboardingFragment
+import java.lang.IllegalArgumentException
+
+class fragmentOnboardingAdapter(fragmentActivity: OnBoardingActivity) :
+    FragmentStateAdapter(fragmentActivity) {
+
+    override fun getItemCount(): Int = 3
+
+
+    override fun createFragment(position: Int): Fragment {
+        return when (position) {
+            0 -> FirstOnboardingFragment.newInstance()
+            1 -> SecondOnboardingFragment.newInstance()
+            2 -> ThirdOnboardingFragment.newInstance()
+            else -> throw IllegalArgumentException("Invalid position: $position")
+        }
+    }
+}

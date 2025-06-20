f5379579bda4810d73ceb1b5c9c2a220212cb12c
wan_m <morozko889@gmail.com>
2025-06-21 00:08:27 +0300

    dz_7_nav_component

--- app/src/main/java/com/example/firstproject/AppActivity.kt
+++ app/src/main/java/com/example/firstproject/AppActivity.kt
@@ -1,24 +1,31 @@
 package com.example.firstproject
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import com.example.firstproject.databinding.ActivityMainBinding
 import com.example.firstproject.page.OnBoardingFragment
 
 class AppActivity : AppCompatActivity() {
 
-    private var binding : ActivityMainBinding? = null
+    private var binding: ActivityMainBinding? = null
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding!!.root)
 
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, OnBoardingFragment.newInstance())
-                .commit()
-        }
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 
     override fun onDestroy() {

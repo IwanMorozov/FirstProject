1fd440faba9a941af8b9d31b2e6b99810deda9a0
Wan <morozko889@gmail.com>
2025-07-24 12:13:32 +0300

    nav_component_correction

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,4 +59,5 @@ dependencies {
     implementation(libs.androidx.fragment.ktx)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.androidx.recyclerview)
 }
